9f44ed17abd9a93632c93cfabb07b953bb5a590e
Erik Töger <erik.toger@gmail.com>
2022-07-19 09:21:39 +0200

    Add menu with routes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
+mod menu;
+mod routes;
 mod stats_preview_card;
+
+use menu::Menu;
+use routes::Route;
 use stats_preview_card::StatsPreviewCard;
+use yew::functional::*;
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+fn switch(routes: &Route) -> Html {
+    match routes {
+        Route::Home => html! {<Menu/>},
+        Route::StatsPreviewCard => html! { <StatsPreviewCard/>},
+    }
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={Switch::render(switch)} />
+        </BrowserRouter>
+    }
+}
 
 fn main() {
-    yew::start_app::<StatsPreviewCard>();
+    yew::start_app::<App>();
 }
---
+++ src/menu/mod.rs
@@ -0,0 +1,59 @@
+use super::routes::Route;
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+#[derive(Properties, Clone, PartialEq)]
+struct CardProps {
+    title: String,
+    difficulty: String,
+    image_url: String,
+    route: Route,
+}
+
+struct Card;
+
+impl Component for Card {
+    type Message = ();
+    type Properties = CardProps;
+    fn create(ctx: &Context<Self>) -> Self {
+        ctx.props();
+        Self
+    }
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let history = ctx.link().history().unwrap();
+        let route = ctx.props().route.clone();
+        let onclick = Callback::once(move |_| history.push(route));
+        html! {
+            <div onclick={onclick} class="card">
+                <div class="text">
+                    <span class="title">{&ctx.props().title}</span>
+                    <span>{&ctx.props().difficulty}</span>
+                </div>
+                <img src={ctx.props().image_url.clone()} alt={ctx.props().title.clone()}/>
+            </div>
+        }
+    }
+}
+
+pub struct Menu;
+
+impl Component for Menu {
+    type Message = ();
+    type Properties = ();
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <div class="menu">
+                <div class="container">
+                <Card title={"Stats Preview Card"}
+                      difficulty="Newbie"
+                      image_url="images/previews/stats_preview_card.jpg"
+                      route={Route::StatsPreviewCard} />
+                </div>
+            </div>
+        }
+    }
+}
